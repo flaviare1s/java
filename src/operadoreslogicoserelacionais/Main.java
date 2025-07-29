b17e4578f690fdfd22adcc54c6b7babe44affac1
flaviare1s <flaviarster@gmail.com>
2025-07-29 08:45:24 -0300

    Melhora o código

--- src/operadoreslogicoserelacionais/Main.java
+++ src/operadoreslogicoserelacionais/Main.java
@@ -5,14 +5,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n1;
-        int n2;
-        System.out.println("Digite o primeiro número: ");
-        n1 = scanner.nextInt();
-        System.out.println("Digite o segundo número: ");
-        n2 = scanner.nextInt();
-        boolean result = (n1 > n2) || (n2 < n1) ? true : false; // Operador ternário
-        System.out.println(result);
+        int idade;
+        String emancipado;
+        boolean podeDirigir;
 
+        System.out.print("Qual a sua idade?");
+        idade = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Você é emancidado? (s/n)");
+        emancipado = String.valueOf(scanner.next().charAt(0));
+
+        podeDirigir = (idade >= 18) || (idade >= 16 && emancipado.equalsIgnoreCase("s"));
+
+        System.out.println(podeDirigir ? "Pode dirigir" : "Não pode dirigir");
     }
 }
