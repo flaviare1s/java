437dff8302968c9a8768db0828a93a1311b4a21b
flaviare1s <flaviarster@gmail.com>
2025-08-06 12:42:36 -0300

    Exercício arrays

---
+++ src/exercicio1/Main.java
@@ -0,0 +1,12 @@
+package exercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        String [] objetos = {"garrafa","copo","litro"};
+
+        for (int tam = objetos.length - 1; tam >= 0; tam--){
+
+            System.out.print(objetos[tam] + " ");
+        }
+    }
+}
